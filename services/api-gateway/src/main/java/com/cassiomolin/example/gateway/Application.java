70182186f0ec144753edc25ba8548d0f206109be
Cassio Mazzochi Molin <cassio.molin@gmail.com>
2017-12-15 12:00:05 +0000

    Improve configuration.

--- services/api-gateway/src/main/java/com/cassiomolin/example/gateway/Application.java
+++ services/api-gateway/src/main/java/com/cassiomolin/example/gateway/Application.java
@@ -1,10 +1,7 @@
 package com.cassiomolin.example.gateway;
 
-import com.cassiomolin.example.gateway.filter.CustomLocationRewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Spring Boot application entry point.
@@ -12,15 +9,9 @@ import org.springframework.context.annotation.Bean;
  * @author cassiomolin
  */
 @SpringBootApplication
-@EnableZuulProxy
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CustomLocationRewriteFilter locationRewriteFilter() {
-        return new CustomLocationRewriteFilter();
     }
 }
---
+++ services/api-gateway/src/main/java/com/cassiomolin/example/gateway/config/ZuulProxyConfiguration.java
@@ -0,0 +1,16 @@
+package com.cassiomolin.example.gateway.config;
+
+import com.cassiomolin.example.gateway.filter.CustomLocationRewriteFilter;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableZuulProxy
+public class ZuulProxyConfiguration {
+
+    @Bean
+    public CustomLocationRewriteFilter locationRewriteFilter() {
+        return new CustomLocationRewriteFilter();
+    }
+}
