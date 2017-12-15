cb3378239b45008ac747ed5ba6df4ddb8f49dcf9
Cassio Mazzochi Molin <cassio.molin@gmail.com>
2017-12-15 16:42:01 +0000

    Configure cache expiration.

--- services/shopping-list-service/src/main/java/com/cassiomolin/example/shoppinglist/config/CachingConfiguration.java
+++ services/shopping-list-service/src/main/java/com/cassiomolin/example/shoppinglist/config/CachingConfiguration.java
@@ -1,19 +1,35 @@
 package com.cassiomolin.example.shoppinglist.config;
 
+import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @EnableCaching
-public class CachingConfiguration {
+public class CachingConfiguration extends CachingConfigurerSupport {
 
     public static final String PRODUCTS_CACHE = "products";
 
     @Bean
+    @Override
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager(PRODUCTS_CACHE);
+
+        return new ConcurrentMapCacheManager() {
+
+            @Override
+            protected Cache createConcurrentMapCache(final String name) {
+                return new ConcurrentMapCache(name, CacheBuilder.newBuilder()
+                        .expireAfterWrite(30, TimeUnit.SECONDS)
+                        .maximumSize(100).build().asMap(), true);
+            }
+        };
     }
 }
